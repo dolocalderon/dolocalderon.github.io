207113c458bb8e67de5878dae710915ace9c13b6
dolocalderon <dolores2001@hotmail.com>
2024-07-13 17:24:52 -0300

    actualizacion portfolio

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //menu responsive
-function responsiveMenu(){
-    var x =document.getElementById("nav");
-    if(x.className === "")
-    {
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
         x.className = "responsive";
     }
-    else{
+    else {
         x.className = "";
     }
 }
 
 //menu responsive seleccionado
-function seleccionar(link){
+function seleccionar(link) {
     var opciones = document.querySelectorAll('#links a');
     opciones[0].className = "";
     opciones[1].className = "";
@@ -26,16 +25,15 @@ function seleccionar(link){
 
 
 //barra de habilidades
-window.onscroll = function(){
+window.onscroll = function () {
     efectoHabilidades()
 };
 
-function efectoHabilidades(){
+function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight = skills.getBoundingClientRect().top;
 
-    if(distancia_skills >= 300)
-    {
+    if (distancia_skills >= 300) {
         document.getElementById("c").classList.add("barra-progreso1");
         document.getElementById("html").classList.add("barra-progreso2");
         document.getElementById("java").classList.add("barra-progreso3");
@@ -44,3 +42,62 @@ function efectoHabilidades(){
         document.getElementById("sql").classList.add("barra-progreso6");
     }
 }
+
+
+//MODAL PROYECYOS
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener los elementos
+    var modal = document.getElementById("myModal");
+    var proyectos = document.querySelectorAll(".proyecto");
+    var closeBtn = document.querySelector(".closeBtn");
+
+    var modalTitle = document.getElementById("modal-title");
+    var modalPrincipalTitle = document.getElementById("modal-principalTitle");
+    var modalLink = document.getElementById("modal-link");
+    var modalSite = document.getElementById("modal-site");
+    var modalDescription = document.getElementById("modal-description");
+    var modalImage = document.getElementById("modal-image");
+    var siteContainer = modalSite.parentElement; //contenedor para sitio
+
+    // Abro el modal al hacer clic en cualquier proyecto
+    proyectos.forEach(function (proyecto) {
+        proyecto.addEventListener("click", function () {
+            var title = proyecto.getAttribute("data-title");
+            var principalTitle = proyecto.getAttribute("data-principalTitle");
+            var link = proyecto.getAttribute("data-link");
+            var site = proyecto.getAttribute("data-site");
+            var description = proyecto.getAttribute("data-description");
+            var image = proyecto.getAttribute("data-image");
+            
+            if (site) {
+                modalSite.href = site;
+                siteContainer.style.display = "block"; 
+            } else {
+                siteContainer.style.display = "none"; // Ocultar el contenedor del enlace
+            }
+
+
+            modalTitle.textContent = title;
+            modalPrincipalTitle.textContent = principalTitle;
+            modalLink.href = link;
+            modalSite.href = site;
+            modalDescription.textContent = description;
+            modalImage.src = image;
+
+            modal.style.display = "block";
+        });
+    });
+
+    // Cierro el modal al hacer clic en el botón de cerrar 
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    // Cerrar el modal al hacer clic fuera del contenido del modal
+    window.addEventListener("click", function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
